1358a50e576ff5b692e4d49c61d45ec0d6d74fb3
Juan <enrique.sanchez@upeu.edu.pe>
2025-12-12 15:52:19 -0500

    chi cheñol

--- src/native-test/java/upeu/edu/pe/ExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package upeu.edu.pe;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ExampleResourceIT {
-    // Execute the same tests but in packaged mode.
-    // TODO: Create ExampleResourceTest base class or implement tests here
-}
